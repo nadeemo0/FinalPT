dcbb331da5df92c268f5c7f6774c7694ab244755
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-04-03 11:49:32 +0000

    primary functions added

--- Bookmarks.py
+++ Bookmarks.py
@@ -1,15 +1,20 @@
 import os #to use history tracked on a doc
 
 #create history file
+def create_file():
+
 
 #add bookmark function
+def add_bookmark():
+
 
 #Display bookmarks
+def display_bookmarks():
 
 #delete bookmarks
+def clear_bookmarks():
 
 #main function
-
 def main():
   
   

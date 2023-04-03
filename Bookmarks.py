a390a805dd411562200af1e58c02b79f04197a9b
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-04-03 11:59:17 +0000

    created outline of main function

--- Bookmarks.py
+++ Bookmarks.py
@@ -2,7 +2,7 @@ import os #to use history tracked on a doc
 
 #create history file
 def create_file():
-
+  if not os.path.exists("Bookmarks.txt")
 
 #add bookmark function
 def add_bookmark():
@@ -12,11 +12,27 @@ def add_bookmark():
 def display_bookmarks():
 
 #delete bookmarks
-def clear_bookmarks():
+def clear():
 
 #main function
 def main():
-  
+  create_file()
+
+  while True:
+    print("1. Add Bookmark")
+    print("2. Display Bookmarks")
+    print("3. Clear Bookmarks")
+    print("4. Exit")
+    choice = input("option:")
+
+    if choice == "1":
+      add_bookmark()
+    if choice == "2":
+      display_bookmarks()
+    if choice == "3":
+      clear()
+    if choice == "4":
+      break
   
 if __name__ == "__main__":
   main()

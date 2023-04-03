f43eca3fdd9075a73cdb2d5be161e53da592d48f
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-04-03 14:35:52 +0300

    Create Bookmarks.py

---
+++ Bookmarks.py
@@ -0,0 +1,17 @@
+import os #to use history tracked on a doc
+
+#create history file
+
+#add bookmark function
+
+#Display bookmarks
+
+#delete bookmarks
+
+#main function
+
+def main():
+  
+  
+if __name__ == "__main__":
+  main()

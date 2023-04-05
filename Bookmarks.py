bb1a0dc4f25ad50ee6ef2156fd286b79e25a8767
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-04-05 10:14:08 +0000

    moving screen stuff and sections idea

--- Bookmarks.py
+++ Bookmarks.py
@@ -1,6 +1,11 @@
 import os
 import time #to use history tracked on a doc
 
+#abstracting moving text
+def moving(message, speed):
+  for char in message:
+    print(char, end='', flush = True)
+    time.sleep(speed)
 #create history file
 def create_file():
   if not os.path.exists("bookmarks.txt"):
@@ -10,15 +15,16 @@ def create_file():
 #add bookmark function
 def add_bookmark():
   while True:
+    section = input("Enter a Section:")
     topic = input("Enter topic:")
     url = input("Enter Url:")
     note = input("Enter a note:")
 
     #append the new bookmark to the file
     with open("bookmarks.txt", "a") as f:
-      f.write(f"{topic}\t{url}\t{note}\n")
+      f.write(f"{section}\t{topic}\t{url}\t{note}\n")
 
-    print(f"Added {topic}")
+    print(f"Added {section}: {topic}")
     break
 
 #Display bookmarks
@@ -31,22 +37,28 @@ def display_bookmarks():
     print("You dont have any stored bookmarks")
   else:
     for bookmark in bookmarks:
-      topic, url, note = bookmark.split("\t")
-      print(f"{topic.capitalize()}: {url} - {note}\n")
+      section, topic, url, note = bookmark.split("\t")
+      print(f"{section.capitalize()}: {topic} {url} {note}\n")
 
-#delete bookmarks
+#delete all bookmarks
 def clear():
   while True:
-    choice = input("Are you sure you want to clear all saved bookmarks(y/n):")
-    if choice == "n":
-      break
-    elif choice == "y":
-      with open("bookmarks.txt", "w") as f:
-        f.write("")
-        print("All Bookmarks Cleared")
+    print("Which do you want to clear:\n1. By section \n2. All")
+    choice = input("Which do you want to clear:")
+    
+    if choice == "2":
+      sure = "Are you sure? (y/n):"
+      moving(sure, 0.01)
+      input("")
+      if sure.lower() == "y":
+        with open("bookmarks.txt", "w") as f:
+          f.write("")
+          print("All bookmarks Cleared")
+          break
+      else:
         break
     else:
-      print("Invalid choice please try again")
+      print("Invalid choice, Try again")
   
 def main():
   # creates file if not there
@@ -54,11 +66,11 @@ def main():
 
   while True:
     time.sleep(1)
-    print("1. Add Bookmark")
-    print("2. Display Bookmarks")
-    print("3. Clear Bookmarks")
-    print("4. Exit")
-    choice = input("Option:")
+    # Moving message
+    m1 = "1. Add Bookmark\n2. Diplay Bookmarks\n3. Clear Bookmarks\n4. Display by secton\n5. Exit\n\nOption:"
+    moving(m1, 0.01)
+
+    choice = input("")
 
     if choice == "1":
       add_bookmark()
@@ -66,7 +78,8 @@ def main():
       display_bookmarks()
     if choice == "3":
       clear()
-    if choice == "4":
+
+    if choice == "5":
       break
   
 if __name__ == "__main__":

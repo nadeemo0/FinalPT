cae21bba9a108681d76b86cf4c9d73762da26947
nadeemo0 <112538997+nadeemo0@users.noreply.github.com>
2023-04-12 07:04:37 +0000

    new changes

--- Bookmarks.py
+++ Bookmarks.py
@@ -24,9 +24,14 @@ def add_bookmark():
     #append the new bookmark to the file
     with open("bookmarks.txt", "a") as f:
       f.write(f"{section}\t{topic}\t{url}\t{note}\n")
-
     print(f"Added {section}: {topic}")
-    break
+
+  #Ask if they would like to add another
+    m6 = "Do you want to add another bookmark?"
+    moving(m6,0.1)
+    c = input("")
+    if c.lower() != "y":
+      break
 
 #Display bookmarks
 def display_bookmarks():
@@ -39,18 +44,34 @@ def display_bookmarks():
   else:
     for bookmark in bookmarks:
       section, topic, url, note = bookmark.split("\t")
-      print(f"{section.capitalize()}: {topic} {url} {note}\n")
+      print(f"{section.capitalize()}: {topic} {url}\n")
+      if note:
+        print(f"Note: {note}")
+      else:
+        print("\n")
+
+def disp_top():
+  with open("bookmarks.txt", "r") as f:
+    bookmarks = f.read().splitlines()
+
+  if len(bookmarks) == 0:
+    mes = "No bookmarks found."
+    moving(mes, 0.1)
+  else:
+    topics =set([bookmark.split("\t")[1]] for bookmark in bookmarks)
+    print("\nTopics")
+    for topic in sorted(topics):
+      print(f"- {topic}")
 
 #delete all bookmarks
 def clear():
   while True:
-    m4 = "Which do you want to clear:\n1. By section \n2. All"
+    m4 = "Are you sure you want to clear all Messages"
     moving(m4, 0.1)
-    choice = input("Which do you want to clear:")
-    
-    if choice == "1":
-
-    if choice == "2":
+    choice = input("(y/n)")
+    if choice == "n":
+      break
+    if choice == "y":
       sure = "Are you sure? (y/n):"
       moving(sure, 0.01)
       input("")
@@ -58,7 +79,7 @@ def clear():
         with open("bookmarks.txt", "w") as f:
           f.write("")
           m5 = "All bookmarks Cleared"
-          moving(m, 0.1)
+          moving(m5, 0.05)
           break
       else:
         break
@@ -76,21 +97,21 @@ def by_sec():
     sections = ["Health", "Arabic", "History", "Math", "Bio", "English", "CSP", "ST"]
     for i, section in enumerate(sections):
       print(f"{i+1}. {section.capitalize()}")
-      m3 = "Enter section number:"
-      moving(m3, 0.2)
-      choice = input("")
-      sec_bkmrk = [bookmark for bookmark in bookmarks if bookmark.startswith(f"{sections[int(choice)-1]}\t")]
-      if len(sec_bkmrk) > 0:
-        print(f"\n{sections[int(choice)-1].capitalize()} bookmarks:")
-            for bookmark in sec_bkmrk:
-                _, topic, url, note = bookmark.split("\t")
-                print(f"{topic} - {url}")
-                if note:
-                    print(f"  Note: {note}\n\n")
-                else:
-                    print("\n\n")
+    m3 = "Enter section number:"
+    moving(m3, 0.2)
+    choice = input("")
+    sec_bkmrk = [bookmark for bookmark in bookmarks if bookmark.startswith(f"{sections[int(choice)-1]}\t")]
+    if len(sec_bkmrk) > 0:
+      print(f"\n{sections[int(choice)-1].capitalize()} bookmarks:")
+      for bookmark in sec_bkmrk:
+        _, topic, url, note = bookmark.split("\t")
+        print(f"{topic} - {url}")
+        if note:
+          print(f"  Note: {note}\n\n")
         else:
-            print("No bookmarks found for the selected section.")
+          print("\n\n")
+    else:
+      print("No bookmarks found for the selected section.")
   
 def main():
   # creates file if not there
@@ -99,8 +120,8 @@ def main():
   while True:
     time.sleep(1)
     # Moving message
-    m1 = "1. Add Bookmark\n2. Diplay Bookmarks\n3. Clear Bookmarks\n4. Display by secton\n5. Exit\n\nOption:"
-    moving(m1, 0.01)
+    m1 = "1. Add Bookmark\n2. Diplay Bookmarks\n3. Clear Bookmarks\n4. Display by secton\n5. Display topic list\n6. Exit\n\nOption:"
+    moving(m1, 0.05)
 
     choice = input("")
 
@@ -110,10 +131,12 @@ def main():
       display_bookmarks()
     if choice == "3":
       clear()
-    if choice --"4":
+    if choice == "4":
       by_sec()
     if choice == "5":
+      disp_top()
+    if choice == "6":
       break
-  
+
 if __name__ == "__main__":
   main()
